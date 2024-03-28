98a08f32ee5d6c47e92cff7e0f15fc8fa59bd591
Rafey Ahmad <rafeyahmad@protonmail.com>
2024-03-28 17:07:34 +0530

    test

--- app/server.go
+++ app/server.go
@@ -15,7 +15,7 @@ func main() {
 
 	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
 
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Pat))
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
